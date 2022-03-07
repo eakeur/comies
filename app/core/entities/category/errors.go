7b9a0f47a1ce573ff34a94ef1adc6606878ff1be
Igor <igor.reisleandro@gmail.com>
2022-03-07 16:46:55 -0300

    feat: created first errors

--- app/core/entities/category/errors.go
+++ app/core/entities/category/errors.go
@@ -3,6 +3,11 @@ package category
 import "errors"
 
 var (
+	ErrInvalidCode = errors.New("the code is not valid. please provide one longer than 2 (two) and shorter than 12 (twelve) characters")
+
 	ErrInvalidName = errors.New("the name is not valid. please provide one longer than 2 (two) and shorter than 60 (sixty) characters")
 
+	ErrNotFound = errors.New("the category searched does not exist or could not be found")
+
+	ErrAlreadyExist = errors.New("the category you are trying to create already exists.")
 )
