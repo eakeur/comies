26cac0cdd3d212040c1a896eeafff51609cea1a6
Igor <igor.reisleandro@gmail.com>
2022-04-27 19:15:44 -0300

    feat: add actions

--- app/core/entities/order/actions.go
+++ app/core/entities/order/actions.go
@@ -10,15 +10,21 @@ type Actions interface {
 	CreateOrder(ctx context.Context, o Order) (Order, error)
 	CreateItem(ctx context.Context, item Item) (Item, error)
 	CreateContent(ctx context.Context, c ...Content) ([]Content, error)
+
 	ListOrders(ctx context.Context) ([]Order, int, error)
 	ListItems(ctx context.Context, orderUID types.UID) ([]Item, error)
 	ListContent(ctx context.Context, itemUID types.UID) ([]Content, error)
+
 	GetOrder(ctx context.Context, uid types.UID) (Order, error)
+
 	UpdateItemStatus(ctx context.Context, uid types.UID, status ItemStatus) error
 	UpdateOrderStatus(ctx context.Context, uid types.UID, status Status) error
+
 	UpdateOrder(ctx context.Context, o Order) error
 	UpdateItem(ctx context.Context, i Item) error
 	UpdateContent(ctx context.Context, c Content) error
+
 	RemoveOrder(ctx context.Context, o Order) error
 	RemoveItem(ctx context.Context, uid types.UID) error
+	RemoveContent(ctx context.Context, uid types.UID) error
 }
