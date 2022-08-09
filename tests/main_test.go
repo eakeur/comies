c71d80e67787a04a7aa3691a2ddd6fa8dbe8f01d
Igor Reis <igor.reisleandro@gmail.com>
2022-08-09 11:21:27 -0300

    feat: readded integration tests

---
+++ tests/main_test.go
@@ -0,0 +1,68 @@
+package tests
+
+import (
+	"comies/app"
+	"comies/app/gateway/api"
+	"comies/app/gateway/persistence/postgres/tests"
+	"context"
+	"fmt"
+	"github.com/bwmarrin/snowflake"
+	"go.uber.org/zap"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	tests.SetupTest(m)
+}
+
+func NewTestApp(t *testing.T, defaultRoute string) Client {
+	ctx := context.Background()
+	db := tests.NewTestDatabase(t, ctx)
+
+	flake, err := snowflake.NewNode(1)
+	if err != nil {
+		log.Fatalf("Could not create snowflake node: %v", err)
+	}
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("Could not create logger instance: %v", err)
+	}
+
+	application := app.NewApplication(app.Gateways{
+		Logger:        logger.Sugar(),
+		Database:      db.Pool,
+		SnowflakeNode: flake,
+	})
+
+	lis, err := net.Listen("tcp", "localhost:")
+	if err != nil {
+		log.Fatalf("Could not open test server: %v", err)
+	}
+
+	addr := lis.Addr()
+
+	t.Cleanup(func() {
+		db.Drop()
+		_ = lis.Close()
+	})
+
+	go http.Serve(lis, api.NewAPI(application))
+
+	origin, err := url.Parse(fmt.Sprintf("http://%s%s", addr, defaultRoute))
+	if err != nil {
+		log.Fatalf("Failed mounting URL: %v", err)
+	}
+
+	return client{
+		client: &http.Client{
+			Timeout: time.Second * 5,
+		},
+		origin: origin,
+	}
+}
